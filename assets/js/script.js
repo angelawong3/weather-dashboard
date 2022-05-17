972295263ded736dab4970583ef63156bcae4ce5
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-05-17 12:40:20 +0100

    added api url and key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const apiKey = "1c6283f007c531f7d629fe699300456e";
+let cityName = "Birmingham";
+
+let requestCurrentUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  cityName +
+  "&appid=" +
+  apiKey;
+let requestForecastUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  cityName +
+  "&appid=" +
+  apiKey;
