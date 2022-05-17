1d0259627663229ff5de2d0c37930521c77fb163
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-05-17 13:54:39 +0100

    assign declaration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+var cityEl = document.getElementById("input-city");
+console.log(cityEl);
+var searchEl = document.getElementById("searchBtn");
+console.log(searchEl);
+
 const apiKey = "1c6283f007c531f7d629fe699300456e";
 let cityName = "Birmingham";
 
