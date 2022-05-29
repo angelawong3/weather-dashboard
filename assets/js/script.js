35570e016a2f6e643c8cdd9eac9cd4e1cc576306
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-05-29 18:52:19 +0100

    click event for search hist

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,46 @@
-var cityEl = document.getElementById("input-city");
-console.log(cityEl);
-var searchEl = document.getElementById("searchBtn");
-console.log(searchEl);
+// global declaration
+const recentSearchHistory = $("#search-history");
 
-const apiKey = "1c6283f007c531f7d629fe699300456e";
-let cityName = "Birmingham";
+const readFromLS = (key, defaultValue) => {
+  // get from LS using key name
+  const dataFromLS = localStorage.getItem(key);
 
-let requestCurrentUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  cityName +
-  "&appid=" +
-  apiKey;
-let requestForecastUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  cityName +
-  "&appid=" +
-  apiKey;
+  // parse data from LS
+  const parsedData = JSON.parse(dataFromLS);
+
+  if (parsedData) {
+    return parsedData;
+  } else {
+    return defaultValue;
+  }
+};
+
+const renderRecentSearches = () => {
+  // get recent searches from LS
+  const recentSearches = readFromLS("recentSearches", []);
+
+  // ["Birmingham", "London"]
+  if (recentSearches.length) {
+    // if render recent searches
+  } else {
+    // else show alert
+    const alert = `<div class="alert alert-info text-center" role="alert">
+    No search history!
+  </div>`;
+
+    // append to parent
+    recentSearchHistory.append(alert);
+  }
+};
+
+const handleRecentSearchClick = () => {
+  console.log("Clicked");
+};
+
+const onReady = () => {
+  renderRecentSearches();
+};
+
+recentSearchHistory.click(handleRecentSearchClick);
+
+$(document).ready(onReady);
